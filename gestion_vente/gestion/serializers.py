d1c5aaf6cd12c2012e1c72567dd7bcc4263c0fd1
Cipo <cipokibona.ck@gmail.com>
2025-05-22 16:04:56 +0200

    permission_classes sur le model user

--- gestion_vente/gestion/serializers.py
+++ gestion_vente/gestion/serializers.py
@@ -5,4 +5,4 @@ class userSerializer(ModelSerializer):
     
     class Meta:
         model = User
-        fields = ['username','password','email','first_name','last_name','tel','email','imgProfil','is_agent_commercial','is_staff','is_admin','is_respo_pos','is_active']
+        fields = ['id','username','password','email','first_name','last_name','tel','email','imgProfil','is_agent_commercial','is_staff','is_admin','is_respo_pos','is_active']
--- gestion_vente/gestion/views.py
+++ gestion_vente/gestion/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 from authentification.models import User
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from .serializers import userSerializer
 
 class UserAPIView(ModelViewSet):
     serializer_class = userSerializer
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         return User.objects.all()
