3db17524f934a6d3ae76e3ae78679c60f7529622
Cipo <cipokibona.ck@gmail.com>
2025-05-24 10:28:34 +0200

    creation de models product et ses objects

--- gestion_vente/gestion/admin.py
+++ gestion_vente/gestion/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Wallet, TauxEchange, TypeEchange
+from .models import Wallet, TauxEchange, TypeEchange, Product, Distributeur, Client, PointOfSell, AchatProduct, ListProductAchat,  ListWalletAchat, VenteProduct, ListProductVente, ListWalletVente
 
 @admin.register(TypeEchange)
 class CustomTypeEchangeAdmin(admin.ModelAdmin):
@@ -16,3 +16,43 @@ class CustomWalletAdmin(admin.ModelAdmin):
 @admin.register(TauxEchange)
 class TauxEchangeAdmin(admin.ModelAdmin):
     list_display = ['devise','taux','date_start','date_end','is_active']
+    
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','is_active','date']
+
+@admin.register(Distributeur)
+class DistributeurAdmin(admin.ModelAdmin):
+    list_display = ['name','adress','tel','is_active','date']
+    
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['respo','fullName','adress','tel','is_active','date']
+    
+@admin.register(PointOfSell)
+class POSAdmin(admin.ModelAdmin):
+    list_display = ['respo','fullName','adress','tel','is_active','date']
+
+@admin.register(AchatProduct)
+class AchatProductAdmin(admin.ModelAdmin):
+    list_display = ['author','distributeur','depot','montantPaye','is_active','date']
+    
+@admin.register(ListProductAchat)
+class ListProductAchatAdmin(admin.ModelAdmin):
+    list_display = ['product','achat','quantity','pricePerUnitOfficiel','pricePerUnitReel','is_active','date']
+    
+@admin.register(ListWalletAchat)
+class ListWalletAchatAdmin(admin.ModelAdmin):
+    list_display = ['achat','wallet','montant','is_active','date']
+    
+@admin.register(VenteProduct)
+class VenteAdmin(admin.ModelAdmin):
+    list_display = ['author','client','depot','montantPaye','is_active','date']
+    
+@admin.register(ListProductVente)
+class ListProductVenteAdmin(admin.ModelAdmin):
+    list_display = ['product','vente','quantity','pricePerUnitOfficiel','pricePerUnitReel','is_active','date']
+    
+@admin.register(ListWalletVente)
+class ListProductVenteAdmin(admin.ModelAdmin):
+    list_display = ['vente','wallet','montant','is_active','date']
--- gestion_vente/gestion/models.py
+++ gestion_vente/gestion/models.py
@@ -30,6 +30,102 @@ class TauxEchange(models.Model):
     date_start = models.DateTimeField(auto_now_add=True)
     date_end = models.DateTimeField()
     is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.devise
+    
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Distributeur(models.Model):
+    name = models.CharField(max_length=100)
+    adress = models.CharField(max_length=100)
+    tel = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Client(models.Model):
+    respo = models.ForeignKey(User, on_delete=models.CASCADE, related_name='respo_client')
+    fullName = models.CharField(max_length=100)
+    adress = models.CharField(max_length=100)
+    tel = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.fullName
+    
+class PointOfSell(models.Model):
+    respo = models.ForeignKey(User, on_delete=models.CASCADE, related_name='respo_POS')
+    fullName = models.CharField(max_length=100)
+    adress = models.CharField(max_length=100)
+    tel = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.fullName
+    
+    
+class AchatProduct(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_achat')
+    distributeur = models.ForeignKey(Distributeur, on_delete=models.CASCADE, related_name='distributeur_achat')
+    depot = models.ForeignKey(PointOfSell, on_delete=models.CASCADE, related_name='recepteur_achat')
+    montantPaye = models.FloatField()
+    # reste = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+class ListProductAchat(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='list_product_achat')
+    achat = models.ForeignKey(AchatProduct, on_delete=models.CASCADE, related_name='list_achat')
+    quantity = models.IntegerField()
+    pricePerUnitOfficiel = models.FloatField()
+    pricePerUnitReel = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+class ListWalletAchat(models.Model):
+    achat = models.ForeignKey(AchatProduct, on_delete=models.CASCADE, related_name='achat_wallet')
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='list_wallet_achat')
+    montant = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    
+class VenteProduct(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_vente')
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='client_vente')
+    depot = models.ForeignKey(PointOfSell, on_delete=models.CASCADE, related_name='pos_vente')
+    montantPaye = models.FloatField()
+    # reste = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+class ListProductVente(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='list_product_vente')
+    vente = models.ForeignKey(VenteProduct, on_delete=models.CASCADE, related_name='list_vente')
+    quantity = models.IntegerField()
+    pricePerUnitOfficiel = models.FloatField()
+    pricePerUnitReel = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
+    
+class ListWalletVente(models.Model):
+    vente = models.ForeignKey(AchatProduct, on_delete=models.CASCADE, related_name='vente_wallet')
+    wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='list_wallet_vente')
+    montant = models.FloatField()
+    is_active = models.BooleanField(default=True)
+    date = models.DateTimeField(auto_now_add=True)
